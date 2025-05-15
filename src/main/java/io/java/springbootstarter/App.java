621d5d9333f3509d970ce03215824dcee6f1ccf1
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-15 16:24:57 -0400

    Specifying spring boot application in App class

---
+++ src/main/java/io/java/springbootstarter/App.java
@@ -0,0 +1,10 @@
+package io.java.springbootstarter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
