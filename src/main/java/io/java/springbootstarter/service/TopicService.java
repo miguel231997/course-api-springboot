b12c2a075c56d432314f3d8758821e54935af6ef
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-16 14:27:38 -0400

    Adding TopicRepository dependeny injection to TopicService

--- src/main/java/io/java/springbootstarter/service/TopicService.java
+++ src/main/java/io/java/springbootstarter/service/TopicService.java
@@ -1,13 +1,18 @@
 package io.java.springbootstarter.service;
 
 
+import io.java.springbootstarter.Repository.TopicRepository;
 import io.java.springbootstarter.model.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
 
     //returning topics list
     public List<Topic> getAllTopics() {
