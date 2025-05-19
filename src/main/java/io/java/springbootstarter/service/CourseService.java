0e16e3ebe8c2652eb2583bad018a8ab884fd7a4e
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-19 14:45:31 -0400

    Implement actual logic with filtering by topic ID:

--- src/main/java/io/java/springbootstarter/service/CourseService.java
+++ src/main/java/io/java/springbootstarter/service/CourseService.java
@@ -1,11 +1,26 @@
 package io.java.springbootstarter.service;
 
+import io.java.springbootstarter.Repository.CourseRepository;
+import io.java.springbootstarter.model.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CourseService {
 
-    public List<Courses> getAllCourses() {
-        return courses;
+    @Autowired
+    CourseRepository courseRepository;
+
+    public List<Course> getAllCourses(String topicId) {
+        return courseRepository.findAll()
+                .stream()
+                .filter(course -> course.getTopic().getId().equals(topicId))
+                .toList();
+    }
+
+    public void addCourse(Course course) {
+        courseRepository.save(course);
     }
 }
