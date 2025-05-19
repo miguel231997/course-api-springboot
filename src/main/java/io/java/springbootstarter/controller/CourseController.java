d9b87bc10aa8ae907dcbfb361e77bddcef2c5dca
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-19 12:16:27 -0400

    Adding dependency injection in controller for course service

--- src/main/java/io/java/springbootstarter/controller/CourseController.java
+++ src/main/java/io/java/springbootstarter/controller/CourseController.java
@@ -1,11 +1,16 @@
 package io.java.springbootstarter.controller;
 
+import io.java.springbootstarter.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
 
     @RequestMapping("/topics/{id}/courses")
     public List<Courses> getAllCourses(@PathVariable String id) {
