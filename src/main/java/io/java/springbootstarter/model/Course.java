a6c3d7e515c96e51c25f312729f1b974343dd062
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-19 12:36:38 -0400

    Tieing course with a topic using a member variable in course model

---
+++ src/main/java/io/java/springbootstarter/model/Course.java
@@ -0,0 +1,54 @@
+package io.java.springbootstarter.model;
+
+public class Course {
+
+    private String id;
+    private String name;
+    private String description;
+    private String topicId;
+
+    private Topic topic;
+
+    public Course() {
+
+    }
+
+    public Course(String id, String name, String description, String topicId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.topicId = topicId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getTopicId() {
+        return topicId;
+    }
+}
