1b9522d2e0c9e2c6fe58e619857b59df290d1fae
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-19 14:39:56 -0400

    Implementing course repository

--- src/main/java/io/java/springbootstarter/Repository/CourseRepository.java
+++ src/main/java/io/java/springbootstarter/Repository/CourseRepository.java
@@ -1,2 +1,12 @@
-package io.java.springbootstarter.Repository;public interface CourseRepository {
+package io.java.springbootstarter.Repository;
+
+import io.java.springbootstarter.model.Course;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+
+    public List<Course> findByName(String name);
+    public List<Course> findByDescription(String description);
 }
