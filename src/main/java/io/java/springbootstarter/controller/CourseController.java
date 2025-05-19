0d7ff423735dc383aac738e7acedbc55243e2f3e
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-19 14:52:22 -0400

    Fix incorrect method calls and typos

--- src/main/java/io/java/springbootstarter/controller/CourseController.java
+++ src/main/java/io/java/springbootstarter/controller/CourseController.java
@@ -1,10 +1,11 @@
 package io.java.springbootstarter.controller;
 
+import io.java.springbootstarter.model.Course;
+import io.java.springbootstarter.model.Topic;
 import io.java.springbootstarter.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class CourseController {
@@ -13,7 +14,13 @@ public class CourseController {
     private CourseService courseService;
 
     @RequestMapping("/topics/{id}/courses")
-    public List<Courses> getAllCourses(@PathVariable String id) {
+    public List<Course> getAllCourses(@PathVariable String id) {
         return CourseService.getAllCourses(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topic/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.addCourse(course);
     }
 }
